28db8767458ad95c73bb9a1f250cc0352189f91a
Daniel Las Heras <daniellasheras0@gmail.com>
2025-12-10 20:03:20 +0100

    Refactor apply-button.js and fetch-data.js to comment out existing functionality
    - Commented out the job application button functionality in apply-button.js to prevent execution during development.
    - Commented out data fetching and rendering logic in fetch-data.js to disable data loading and console logging for debugging purposes.
    - Ensured no changes to the overall structure or functionality while preparing for further development.

--- projects/dev-jobs/pages/apply-button.js
+++ projects/dev-jobs/pages/apply-button.js
@@ -1,14 +1,14 @@
-const buttons = document.querySelectorAll(".button-apply-job");
+// const buttons = document.querySelectorAll(".button-apply-job");
 
-const jobListingsSection = document.querySelector(".jobs-listings");
+// const jobListingsSection = document.querySelector(".jobs-listings");
 
-jobListingsSection?.addEventListener("click", (e) => {
+// jobListingsSection?.addEventListener("click", (e) => {
     
-    console.log("click event triggered", e.target);
+//     console.log("click event triggered", e.target);
 
-    if (e.target.classList.contains("button-apply-job")) {
-        e.target.textContent = "Aplicado";
-        e.target.classList.add("is-applied");
-        e.target.disabled = true;
-    }
-});
+//     if (e.target.classList.contains("button-apply-job")) {
+//         e.target.textContent = "Aplicado";
+//         e.target.classList.add("is-applied");
+//         e.target.disabled = true;
+//     }
+// });
